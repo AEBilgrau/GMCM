306744cee30f2f18e5776491b5330369e84987cc
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-01-30 13:27:43 +0100

    Removed ::: calls.

--- R/Uhat.R
+++ R/Uhat.R
@@ -2,5 +2,5 @@ Uhat <- function (x) {  # Ranking function
   if (is.vector(x)) {
     x <- matrix(x, length(x), 1)
   }
-  base::apply(x, 2, rank, ties.method = "max")/(base::nrow(x) + 1)
+  apply(x, 2, rank, ties.method = "max")/(nrow(x) + 1)
 }
