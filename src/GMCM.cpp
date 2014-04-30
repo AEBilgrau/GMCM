d11452b45e9d672744b8d425f9cc7a20862e9d50
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-04-30 19:29:06 +0200

    Added some comment.

--- src/GMCM.cpp
+++ src/GMCM.cpp
@@ -183,17 +183,17 @@ arma::mat pgmm_marginal(arma::mat& z,
                         Rcpp::List mus, 
                         Rcpp::List sigmas, 
                         NumericVector pie) {
-  int d = mus.size();
-  int n = z.n_rows;
-  int m = z.n_cols;
+  int d = mus.size(); // Nbr of components in mixture (not dimension!)
+  int n = z.n_rows;   // Nbr of observations
+  int m = z.n_cols;   // Dimension (!)
   
   NumericMatrix x = Rcpp::as<Rcpp::NumericMatrix>(wrap(z));
   NumericMatrix tmp(n, m); // Matrix of n rows and m columns (filled with 0)
 
   // Holders for the k'th mu and variance for the j'th marginal
-  NumericVector tmp_mus(m);
+  NumericVector tmp_mus(m); 
   NumericMatrix tmp_sigmas(m, m);
-    
+
   double mu, sd;
   NumericVector xx = no_init(n);
   
@@ -205,7 +205,7 @@ arma::mat pgmm_marginal(arma::mat& z,
       xx = x(_, j);
       mu = tmp_mus(j);
       sd = sqrt(tmp_sigmas(j,j));
-      tmp(_, j) = tmp(_, j) + pie[k]*approx_pnorm(xx, mu, sd);
+      tmp(_, j) = tmp(_, j) + pie[k] * approx_pnorm(xx, mu, sd);
     }
   }
   arma::mat ans(tmp.begin(), n, m, false); 
@@ -213,4 +213,38 @@ arma::mat pgmm_marginal(arma::mat& z,
   return ans;
 }
 
+
+/*** R
+library(GMCM)
+data <- SimulateGMCMData(n = 1000, d = 4, theta = rtheta(d = 4, m = 3))
+theta <- rtheta(d = 4, m = 3)
+
+aa <- pgmm_marginal(data$z, theta$mu, 
+              theta$sigma, theta$pie)
+
+set.seed(1)
+ 
+ # Choosing the true parameters and simulating data
+ true.par <- c(0.8, 3, 1.5, 0.4)
+ data <- SimulateGMCMData(n = 1000, par = true.par, d = 2)
+ uhat <- Uhat(data$u)  # Observed ranks
+ 
+ # Plot of latent and observed data colour coded by the true component
+ par(mfrow = c(1,2))
+ plot(data$z, main = "Latent data", cex = 0.6,
+      xlab = "z (Experiment 1)", ylab = "z (Experiment 2)", 
+      col = c("red","blue")[data$K])
+ plot(uhat, main = "Observed data", cex = 0.6,
+      xlab = "u (Experiment 1)", ylab = "u (Experiment 2)", 
+      col = c("red","blue")[data$K])
+ 
+ # Fit the model using the Pseudo EM algorithm
+ init.par <- c(0.5, 1, 1, 0.5)
+ res <- GMCM:::PseudoEMAlgorithm(uhat, meta2full(init.par, d = 2), 
+                                 verbose = TRUE,
+                                 convergence.criterion = "absGMCM", 
+                                 eps = 1e-4,
+                                 trace.theta = FALSE, 
+                                 meta.special.case = TRUE)
+*/
 
