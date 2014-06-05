b41251f2ae818405261d8cf3b2ed28db45bf9e54
Anders Ellern Bilgrau <abilgrau@math.aau.dk>
2014-06-05 10:12:20 +0200

    Added ranking for "security".
    If non-ranked data is given as input, the function now ranks it. The Uhat transformation is idempotent. So if ranked data is given, the output is not changed.

--- R/get.idr.R
+++ R/get.idr.R
@@ -1,5 +1,5 @@
 get.idr <- function (x, theta, ...) {
   #pseudo.data <- qgmm.marginal(u = Uhat(x), theta = theta, ...)
   #kappa <- EStep(x = pseudo.data, theta = theta)
-  return(get.prob(x, theta, ...)[,1])
+  return(get.prob(Uhat(x), theta, ...)[,1])
 }
