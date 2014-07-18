05433507fc9f363464cc70a103204d365845ee00
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-07-18 13:43:29 +0200

    EStep now calls EStepRcpp

--- R/EStep.R
+++ R/EStep.R
@@ -1,9 +1,13 @@
 EStep <- function (x, theta) {
-  g <- function(j) {
-    theta$pie[j]*dmvnormal(x, mu = theta$mu[[j]], sigma = theta$sigma[[j]])
-  }
-  f <- sapply(1:theta$m, g)
-  kappa <- f/rowSums(f)
-  kappa[is.nan(kappa[,1]) | is.nan(kappa[,2]), ] <- 0  # What if kappa has more than 3 cols, fix!
-  return(kappa)
+  return(EStepRcpp(x, mus = theta$mu, sigmas = theta$sigma, pie = theta$pie))
 }
+# Old EStep:
+# EStep <- function (x, theta) {
+#   g <- function(j) {
+#     theta$pie[j]*dmvnormal(x, mu = theta$mu[[j]], sigma = theta$sigma[[j]])
+#   }
+#   f <- sapply(1:theta$m, g)
+#   kappa <- f/rowSums(f)
+#   kappa[is.nan(kappa[,1]) | is.nan(kappa[,2]), ] <- 0  # What if kappa has more than 3 cols, fix!
+#   return(kappa)
+# }
