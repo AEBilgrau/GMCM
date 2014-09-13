90c949075086b2f7aceee8dbf95d72f46f8f1951
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-09-13 19:06:35 +0200

    Added tests for rho transform and inv.rho.transform.

--- inst/tests/test-logit-inv.logit.R
+++ inst/tests/test-logit-inv.logit.R
@@ -4,7 +4,7 @@ p <- runif(100)
 a <- GMCM:::logit(p)
 pp <- GMCM:::inv.logit(a)
 
-test_that("inv.logit returns proper formatted output", {
+test_that("inv.logit and logit returns proper formatted output", {
   expect_that(is.numeric(a),  is_true())
   expect_that(length(a),  equals(length(p)))
   expect_that(is.numeric(pp),  is_true())
