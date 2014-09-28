24781219364ac300ce79011bcbf2a7d63e828a25
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2014-09-28 15:34:22 +0200

    Get idr not exported. Updated docs.

--- R/get.idr.R
+++ R/get.idr.R
@@ -1,9 +1,7 @@
 #' @rdname get.IDR
 #' @return \code{get.idr} returns a vector where the i'th entry is the
-#'   posterior probability that observation i is irreproducible.
-#' @export
+#'   posterior probability that observation i is irreproducible. A simple wrapper
+#'   for \code{get.prob}.
 get.idr <- function (x, theta, ...) {
-  #pseudo.data <- qgmm.marginal(u = Uhat(x), theta = theta, ...)
-  #kappa <- EStep(x = pseudo.data, theta = theta)
-  return(get.prob(Uhat(x), theta, ...)[,1])
+  return(get.prob(Uhat(x), theta, ...)[, 1])
 }
