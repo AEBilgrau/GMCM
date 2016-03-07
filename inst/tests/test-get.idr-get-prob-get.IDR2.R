7a10ac80761630d302d38a5d1c1d7a5688335149
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2016-03-07 19:29:33 +0100

    Changes for testthat v0.12.0. Rewrote custom is_between() to new expect_between().

--- inst/tests/test-get.idr-get-prob-get.IDR2.R
+++ inst/tests/test-get.idr-get-prob-get.IDR2.R
@@ -1,39 +1,41 @@
 context("Check get.idr, get.IDR, get.prob")
 
 
-#' Tests if the returned value(s) within a range
+#' Tests if value within a range
 #'
 #' This function tests if the range of the returned values is within
 #' the interval from \code{a} to \code{b}.
 #'
-#' @param a The lower limit.
-#' @param b The upper limit.
+#' @param object The object to be tested.
+#' @param lower The lower limit.
+#' @param upper The upper limit.
+#' @param included Specifies which limits are included in the interval.
 #' @author
 #'   Anders Ellern Bilgrau <abilgrau(at)math.aau.dk> \cr
-#'   (rewritten from  functions in the \testthat package by Hadley Wickham)
 #' @seealso \code{\link{is_less_than}} \code{\link{is_more_than}}
 #' @examples
 #' library("testthat")
-#' expect_that(runif(100), is_between(0, 1))
-#' expect_that(rnorm(100), is_between(-2, 2))
-is_between <- function(a, b, label = NULL, ...) {
-  if (is.null(label)) {
-    label <- find_expr("expected")
+#' expect_between(runif(1), 0, 1)
+#' expect_between(rnorm(1), -2, 2)
+#' expect_between(2, -2, 2, included = "upper")
+#' expect_between(2, -2, 2, included = "none")
+expect_between <- function(object, lower = 0, upper = 1,
+                           included = c("both", "lower", "upper", "none")) {
+  included <- match.arg(included)
+  if (included %in% c("both", "upper")) {
+    act1 <- expect_lte(max(object), upper)
+  } else {
+    act1 <- expect_lt(max(object), upper)
   }
-  else if (!is.character(label) || length(label) != 1) {
-    label <- deparse(label)
+  if (included %in% c("both", "upper")) {
+    act2 <- expect_gte(min(object), lower)
+  } else {
+    act2 <- expect_gt(min(object), lower)
   }
-  function(actual) {
-    r <- range(actual)
-    expectation(a <= r[1] && r[2] <= b,
-                paste0("not between ", label, ". Range is from ",
-                       format(r[1]), " to ", format(r[2]), "."),
-                paste0("is between ", label))
+  if (exists("act1") && exists("act2") && identical(act1, act2)) {
+    return(invisible(act1))
   }
 }
-environment(is_between) <- asNamespace('testthat')
-
-
 
 true.par <- c(0.9, 2, 0.7, 0.6)
 n <- 1000
@@ -41,7 +43,7 @@ data <- SimulateGMCMData(n = n, par = true.par, d = 2)
 
 l <- 0.5
 res1 <- get.IDR(data$u, true.par, threshold = l)
-res2 <- get.idr(data$u, data$theta)
+res2 <- GMCM:::get.idr(data$u, data$theta)
 res3 <- get.prob(data$u, data$theta)
 
 
@@ -54,20 +56,23 @@ test_that("test get.IDR result format", {
   # idr
   expect_that(is.numeric(res1$idr), is_true())
   expect_that(length(res1$idr), equals(n))
-  expect_that(res1$idr, is_between(0, 1))
+  expect_between(res1$idr, 0, 1)
+
   # IDR
   expect_that(is.numeric(res1$IDR), is_true())
   expect_that(length(res1$IDR), equals(n))
-  expect_that(res1$IDR, is_between(0, 1))
+  expect_between(res1$IDR, 0, 1)
+
   # l
   expect_that(is.numeric(res1$l), is_true())
   expect_that(length(res1$l), equals(1))
   expect_that(res1$l, equals(sum(res1$IDR < l)))
-  expect_that(res1$l, is_between(0, n))
+  expect_between(res1$l, 0, n)
+
   # Khat
   expect_that(is.numeric(res1$Khat), is_true())
   expect_that(length(res1$Khat), equals(n))
-  expect_that(res1$Khat, is_between(1, data$theta$m))
+  expect_between(res1$Khat, 1, data$theta$m)
 })
 
 
@@ -75,16 +80,16 @@ test_that("test get.idr result format", {
   # res2
   expect_that(is.numeric(res2), is_true())
   expect_that(length(res2), equals(n))
-  expect_that(res2, is_between(0, 1))
+  expect_between(res2, 0, 1)
 })
 
 
 test_that("test get.prob result format", {
-  #res3
+  # res3
   expect_that(is.numeric(res3), is_true())
   expect_that(is.matrix(res3), is_true())
   expect_that(dim(res3), equals(c(n, data$theta$m)))
-  expect_that(res3, is_between(0, 1))
+  expect_between(res3, 0, 1)
 })
 
 
