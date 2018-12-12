58b86ae29a3063f00cd489d3f135f455f87d3161
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2018-12-13 00:27:48 +0100

    Skip test on CRAN to be sure to meet time limit for NOTE <10s

--- tests/testthat/test-plot.theta.R
+++ tests/testthat/test-plot.theta.R
@@ -4,6 +4,7 @@ theta <- rtheta(m = 2, d = 2)  # Create a random correctly formatted theta
 file <- tempfile(fileext = ".pdf")
 
 test_that("plotting theta objects works", {
+  skip_on_cran()
   pdf(file)
   out <- plot(theta, add.ellipses = TRUE)
   dev.off()
