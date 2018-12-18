164d1a276b98b0e2d8ae6483fed85cf8486ff0c2
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2018-12-18 20:53:41 +0100

    Better handling of issue #10. Improved related docs. Added default heuristic theta in EMAlgorithm if not provided.

--- R/EMAlgortihm.R
+++ R/EMAlgortihm.R
@@ -3,12 +3,17 @@
 #' The regular expectation-maximization algorithm for general multivariate
 #' Gaussian mixture models.
 #'
-#' Though not as versatile, the algorithm can be a faster alternative to
-#' \code{Mclust} in the \code{mclust}-package.
+#' @details Though not as versatile, the algorithm can be a faster alternative to
+#' \code{Mclust} in the \code{mclust}-package. If \code{theta} is not given,
+#' a k-means clustering is used to determine the initial \code{theta}.
 #'
 #' @param x A matrix of observations where rows correspond to features and
 #'   columns to experiments.
-#' @param theta A list of parameters as described in \code{\link{rtheta}}.
+#' @param theta A list of parameters of class \code{theta} as described in
+#'   \code{\link{rtheta}}.
+#'   Optional. If not provided \code{m} should be given.
+#' @param m \code{numeric}. The number of components if \code{theta} is not
+#'   supplied.
 #' @param eps The maximal required difference in successive likelihoods to
 #'   establish convergence.
 #' @param max.ite The maximum number of iterations.
@@ -38,13 +43,34 @@
 #' plot(data$z, cex = 0.5, pch = 16, main = "GMM clustering",
 #'      col = rainbow(3)[apply(res$kappa,1,which.max)])
 #'
-EMAlgorithm <- function (x, theta, eps = 1e-6, max.ite = 1e5,
+EMAlgorithm <- function (x, theta, m, eps = 1e-6, max.ite = 1e5,
                          trace.theta = FALSE, verbose = FALSE) {
+  if (missing(m) && missing(theta)) {
+    stop("Either m or theta should be provided.")
+  }
+  if (!missing(m) && !missing(theta)) {
+    stopifnot(m == theta$m)
+  }
+  if (missing(theta) && !missing(m)) {
+    theta <- rtheta(m = m, d = ncol(x), method = "EqualSpherical")
+    km <- kmeans(x, centers = m)
+    theta$mu[] <- lapply(seq_len(m), function(j) km$centers[j, ])
+    theta$pie[] <- km$size/sum(km$size)
+    theta$sigma[] <- lapply(seq_len(m), function(j) cov(x[km$cluster == m, ]))
+  }
+
   loglik.tr <- c(dgmm.loglik(theta, x))
-  theta.tr  <- vector("list", 1); theta.tr[[1]] <- theta
+  theta.tr  <- vector("list", 1)
+  theta.tr[[1]] <- theta
   for (k in 2:max.ite) {
     kappa <- EStep(x = x, theta = theta)
     theta <- MStep(x = x, kappa = kappa)
+    if (k == 2 && any(low.prob <- colSums(kappa) < 1e-15)) {
+      stop("No observations are estimated to be from component(s): ",
+           paste(which(low.prob), collapse = ", "), ". ",
+           "All posterior probabilities are zero. ",
+           "Try another start estimate or fewer components.")
+    }
     loglik.tr[k] <- dgmm.loglik(theta, x)
     theta.tr[[k]] <- theta
     delta <- loglik.tr[k] - loglik.tr[k-1]
@@ -56,7 +82,7 @@ EMAlgorithm <- function (x, theta, eps = 1e-6, max.ite = 1e5,
     if (delta < eps)
       break
     if (k == max.ite)
-      warning(paste("Max (", max.ite, ") iterations reached", sep = ""))
+      warning(paste0("Max (", max.ite, ") iterations reached"))
   }
   res <- list(theta = theta,
               loglik.tr = loglik.tr,
--- tests/testthat/test-EMAlgorithm.R
+++ tests/testthat/test-EMAlgorithm.R
@@ -6,13 +6,15 @@ n <- 1000
 test_that("EMAlgorithm returns properly formatted output", {
   for (d in 3:4) {
     for (m in 3) {
-      data <- SimulateGMMData(n = n, d = d, m = m)  # Simulate some data
-      start.theta <- rtheta(d = d, m = m)
+      dat <- SimulateGMMData(n = n, d = d, m = m)  # Simulate some data
 
       for (trace_theta in c(TRUE, FALSE)) {
-        res <- GMCM:::EMAlgorithm(data$z, theta = start.theta, max.ite = 100,
-                                  trace.theta = trace_theta)
-
+        suppressWarnings({
+          res <-  GMCM:::EMAlgorithm(dat$z,
+                                     m = m,
+                                     max.ite = 100,
+                                     trace.theta = trace_theta)
+        })
         expect_that(length(res), equals(ifelse(trace_theta, 4L, 3L)))
         expect_that(is.theta(res$theta), is_true())  # theta
         expect_that(is.numeric(res$loglik.tr), is_true())
@@ -20,7 +22,7 @@ test_that("EMAlgorithm returns properly formatted output", {
         expect_that(is.matrix(res$kappa), is_true())
         expect_that(max(res$kappa), is_less_than(1 + 1e-10))
         expect_that(min(res$kappa), is_more_than(0 - 1e-10))
-        expect_that(dim(res$kappa), equals(c(n, start.theta$m)))
+        expect_that(dim(res$kappa), equals(c(n, res$theta$m)))
         expect_that(length(res$theta$mu), equals(m))
         expect_that(length(res$theta$sigma), equals(m))
         expect_that(all(sapply(res$theta.tr, is.theta)), is_true())
@@ -34,18 +36,22 @@ test_that("EMAlgorithm returns properly formatted output", {
 
 m <- 2
 d <- 3
+dat <- SimulateGMMData(n = n, d = d, m = m)  # Simulate some data
+start.theta <- rtheta(d = d, m = m, method = "EqualSpherical")
+km <- kmeans(dat$z, centers = m)
+start.theta$mu[] <- lapply(1:m, function(j) km$centers[j, ])
+start.theta$pie[] <- km$size/sum(km$size)
+start.theta$sigma[] <- lapply(1:m, function(j) cov(dat$z[km$cluster == m, ]))
 
 test_that("EMAlgorithm warns if max iterations is hit", {
-  data <- SimulateGMMData(n = n, d = d, m = m)  # Simulate some data
   expect_warning(
-    GMCM:::EMAlgorithm(data$z, theta = rtheta(d = d, m = m), max.ite = 5)
+    GMCM:::EMAlgorithm(dat$z, theta = start.theta, max.ite = 2)
   )
 })
 
 test_that("EMAlgorithm is verbose if asked", {
-  data <- SimulateGMMData(n = n, d = d, m = m)  # Simulate some data
   expect_output(
-    GMCM:::EMAlgorithm(data$z, theta = rtheta(d = d, m = m), verbose = TRUE)
+    GMCM:::EMAlgorithm(dat$z, theta = start.theta, verbose = TRUE)
   )
 })
 
