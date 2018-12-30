37cc4f731f2c2bb68b288661a8a65c698dcea251
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2018-12-30 15:59:40 +0100

    Added basic UI and user file input handling.

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,26 +1,58 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+
+  # Reactive value containing the data.frame
+  user_data <- reactiveVal()
+
+  # Load input file
+  output$in_file <- renderTable({
+
+    # Set required dependency, input$in_file is NULL initially.
+    req(input$in_file)
+
+    # Load file
+    tryCatch(
+      {
+        user_data(read.table(input$in_file$datapath,
+                      header = input$header,
+                      sep = input$sep,
+                      quote = input$quote))
+      },
+      error = function(e) {
+        # Return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
+
+    return(head(user_data(), n = input$n_rows))
+  })
+
+  output$file_description <- renderUI({
+    # Set required dependency
+    req(usr_df <- user_data())
+
+    tagList(
+      h1("Dataset loaded"),
+      p(sprintf("%i rows and %i columns detected. Showing %i rows below:",
+                nrow(usr_df), ncol(usr_df), input$n_rows)),
+      hr()
+    )
+  })
+
+
+
+
   output$distPlot <- renderPlot({
-    
+
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
+    x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
+
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+
   })
-  
+
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,33 +1,84 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+# Define UI for application
+shinyUI(
+
+  navbarPage(
+    title = "Gaussian Mixture Copula Models",
+    theme = "bootstrap.css",
+
+    # File input tab ----
+    tabPanel(
+      title = "File input",
+
+      sidebarPanel(
+        # File input ----
+        fileInput("in_file", "Choose CSV File",
+                  multiple = FALSE,
+                  accept = c("text/csv",
+                             "text/comma-separated-values,text/plain",
+                             ".csv")),
+
+        p("The input file should be a plain text file where columns",
+          "correspond to features/variables/experiments and rows correspond to",
+          "observations."),
+
+        # Input: Checkbox if file has header ----
+        checkboxInput("header", "Header", TRUE),
+
+        # Input: Select separator ----
+        radioButtons("sep", "Separator",
+                     choices = c(Comma = ",",
+                                 Semicolon = ";",
+                                 Tab = "\t"),
+                     selected = ","),
+
+        # Input: Select quotes ----
+        radioButtons("quote", "Quotes",
+                     choices = c(None = "",
+                                 "Double Quote" = '"',
+                                 "Single Quote" = "'"),
+                     selected = '"'),
+
+        # Input: Select rows to show ----
+        numericInput("n_rows", "Number of rows to display",
+                     value = 20)
+      ),
+
+      htmlOutput("file_description"),
+      tableOutput("in_file")
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+
+
+    # General model tab ----
+    tabPanel(
+      title = "General GMCM",
+
+      # Show a plot of the generated distribution
+      titlePanel("General GMCM for unsupervised clustering"),
+      hr(),
+      sidebarPanel(
+        sliderInput(inputId = "bins", label = "No bins", min = 0, max = 50, value = 10)
+      ),
+      mainPanel(
+        plotOutput("distPlot")
+      )
+    ),
+
+    # Special model tab ----
+    tabPanel(
+      title = "Special GMCM"
+    ),
+
+    navbarMenu(
+      title = "",
+      icon = icon("bars"),
+      #title = "More",
+      tabPanel("Links"),
+      tabPanel("References")
     )
+
+
+
   )
-))
+)
