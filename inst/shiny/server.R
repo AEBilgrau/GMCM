61cfcd83c53044bbcc51bf94e7e41c0ca1bf7b58
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2018-12-31 09:15:18 +0100

    Added basic meta GMCM functionality.

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -1,13 +1,16 @@
 library(shiny)
 library(DT)
+library(GMCM)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-  # Reactive value containing the data.frame
+  # Reactive value containing the data.frame ----
   user_data <- reactiveVal()
 
-  # Load input file
+  # FILE INPUT ____________________________________________________________ ----
+
+  # Load input file ----
   output$in_file_table <- renderDT({
 
     # Set required dependency, input$in_file is NULL initially.
@@ -27,14 +30,19 @@ shinyServer(function(input, output) {
       }
     )
 
-    d_t <- datatable(user_data(),
-                     style = "bootstrap",
-                     class = "display cell-border compact",
-                     selection = list(target = 'column')
-    )
+    d_t <- datatable(
+      user_data(),
+      style = "bootstrap",
+      class = "display cell-border compact",
+      selection = list(target = 'row+column')
+    ) %>%
+      formatSignif(columns = numeric())
+
     return(d_t)
   })
 
+
+  # Create input file text ----
   output$file_description <- renderUI({
     # Set required dependency
     req(usr_df <- user_data())
@@ -47,8 +55,50 @@ shinyServer(function(input, output) {
     )
   })
 
+  output$raw_data_plot <- renderPlot(
+    width = 700, #px
+    height = 700, #px
+    expr = {
+
+    req(user_data())
+
+    # Get data, select
+    d <- user_data()
+    col_sel <- setdiff(input$in_file_table_columns_selected, 0) # Exclude index 0 (rownames)
+    row_sel <- input$in_file_table_rows_selected
+
+    if (is.null(col_sel) || length(col_sel) != 2) {
+      i <- 1
+      j <- 2
+    } else {
+      i <- col_sel[1]
+      j <- col_sel[2]
+    }
+
+    # Colour selected rows
+    cols <- rep("#00000050", nrow(d))
+    cols[row_sel] <- "red"
+
+    # Do plot
+    plot(x = d[, i],
+         y = d[, j],
+         xlab = names(d)[i],
+         ylab = names(d)[j],
+         axes = FALSE,
+         main = "Scatter",
+         col = cols,
+         asp = 1)
+    axis(1)
+    axis(2)
+
+    # Add selected points on top
+    points(d[row_sel, i], d[row_sel, j], col = "red", pch = 16)
+  })
 
 
+
+
+  # GENERAL GMCM __________________________________________________________ ----
 
   output$distPlot <- renderPlot({
 
@@ -59,6 +109,46 @@ shinyServer(function(input, output) {
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
+  })
+
+
+
+
+  # SPECIAL GMCM __________________________________________________________ ----
+
+  # Initalise reative values ----
+  user_data_pre <- reactiveVal()
+  meta_fit <- reactiveVal()
+
+  observeEvent(input$meta_large_vals, {
+    user_data_pre(Uhat(ifelse(input$meta_large_vals, 1, -1) * user_data()))
+  })
+
+  # observe button push and fit model ----
+  observeEvent(input$fit_meta_push, {
+    u <- user_data_pre()
+
+    init.par <- c(pie1  = input$par1,
+                  mu    = input$par2,
+                  sigma = input$par3,
+                  rho   = input$par4)
+    res <- fit.meta.GMCM(u = u,
+                         init.par = init.par,
+                         method = input$meta_method,
+                         max.ite = input$meta_max_ite,
+                         verbose = TRUE,
+                         positive.rho = input$meta_positive_rho,
+                         trace.theta = TRUE)
+    meta_fit(res)
+  })
+
+  output$meta_str <- renderPrint({
+    cat("\n\nstr(meta_fit())\n")
+    str(meta_fit())
+    cat("\n\nstr(input$in_file_table)\n")
+    str(input$in_file_table)
+    cat("\n\nprint(input$in_file_table_columns_selected)\n")
+    print(input$in_file_table_columns_selected)
   })
 
 })
--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,19 +1,23 @@
 library(shiny)
 library(DT)
+library(GMCM)
 
 # Define UI for application
 shinyUI(
 
   navbarPage(
-    title = "Gaussian Mixture Copula Models",
+    title = strong("GMCM"),
+    windowTitle = "GMCM",
     theme = "bootstrap.css",
 
-    # navbarMenu: File input tab ----
+    # navbar: File input tab ----
     tabPanel(
       title = "File input",
 
       sidebarLayout(
         sidebarPanel(
+          width = 3,
+
           # * Input: File ----
           fileInput("in_file", "Choose CSV File",
                     multiple = FALSE,
@@ -30,10 +34,10 @@ shinyUI(
 
           # * Input: select separator ----
           radioButtons("sep", "Separator",
-                       choices = c(Comma = ",",
-                                   Semicolon = ";",
-                                   Tab = "\t"),
-                       selected = ","),
+                       choices = c("Comma" = ",",
+                                   "Semicolon" = ";",
+                                   "Tabs" = "\t"),
+                       selected = ";"),
 
           # * Input: select quotes ----
           radioButtons("quote", "Quotes",
@@ -44,16 +48,20 @@ shinyUI(
 
         ),
 
+        # * Output ----
         mainPanel(
           htmlOutput("file_description"),
-          DTOutput("in_file_table")
+          DTOutput("in_file_table"),
+          column(9, align = "center",
+            plotOutput("raw_data_plot")
+          )
         )
       )
     ),
 
 
 
-    # navbarMenu: General model ----
+    # navbar: General model ----
     tabPanel(
       title = "General GMCM",
 
@@ -70,13 +78,85 @@ shinyUI(
 
 
 
-    # navbarMenu: Special model ----
+    # navbar: Special model ----
     tabPanel(
-      title = "Special GMCM"
+      title = "Special GMCM",
+
+      sidebarLayout(
+        sidebarPanel(
+          width = 3,
+
+          tabsetPanel(
+            type = "tabs",
+
+            # * Input: tab Start parameters ----
+            tabPanel(
+              title = "Start parameters",
+              tags$br(),
+              sliderInput(inputId = "par1",
+                          label = "Mixture proportion of spurious signals",
+                          min = 0, max = 1, value = 0.7, step = 0.01),
+
+              numericInput(inputId = "par2",
+                           label = "Mean value",
+                           min = 0, value = 1),
+
+              numericInput(inputId = "par3",
+                           label = "Standard deviation",
+                           min = 0, value = 1),
+
+              sliderInput(inputId = "par4",
+                          label = "Correlation",
+                          min = 0, max = 1, value = 0.5, step = 0.01)
+
+            ),
+
+            # * Input: tab Addtional parameters ----
+            tabPanel(
+              title = "Addtional fitting parameters",
+
+              tags$br(),
+              selectInput(inputId = "meta_method",
+                          label = "Optimization method",
+                          choices = eval(formals(fit.meta.GMCM)$method)),
+
+              checkboxInput(inputId = "meta_positive_rho",
+                            label = "Enforce positive correlation (rho)",
+                            value = formals(fit.meta.GMCM)$positive.rho),
+
+              numericInput(inputId = "meta_max_ite",
+                           label = "Maximum iterations",
+                           min = 1,
+                           value = formals(fit.meta.GMCM)$max.ite)
+
+            )
+          ),
+
+          # * Input: Large vals => reproducible
+          tags$hr(),
+          strong("Important!"),
+          checkboxInput(inputId = "meta_large_vals",
+                        label = "Larger values in data indicate stronger evidence",
+                        value = TRUE),
+
+          # * Input: Fit model ----
+          tags$hr(),
+          actionButton(inputId = "fit_meta_push",
+                       label = "Fit model",
+                       icon = icon("cog"))
+
+        ),
+
+        # * Output ----
+        mainPanel(
+          h1("Special GMCM for reproducibility analysis"),
+          verbatimTextOutput("meta_str")
+
+        )
+      )
     ),
 
-
-    # navbarMenu: More ----
+    # navbar: More ----
     navbarMenu(
       title = "",
       icon = icon("bars"),
@@ -89,3 +169,4 @@ shinyUI(
 
   )
 )
+
