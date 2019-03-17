03ea68db31687962b304835cf8d93e7d1b3b4e24
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-03-17 19:43:06 +0100

    Add content to 'general model' report.

--- inst/shiny/report_full.R
+++ inst/shiny/report_full.R
@@ -8,9 +8,19 @@
 #'   header: TRUE
 #'   sep: ";"
 #'   quote: '"'
+#'   model_cols: !r c("PreVsPost.Fresh.pval", "PreVsPost.Frozen.pval")
+#'   theta: !r GMCM::rtheta(m = 2, d = 2)
+#'   fit_method: "NM"
+#'   max_ite: 50
+#'   full_class_type: "thres_prob"
+#'   full_thres_prob: 0.9
 #' ---
 
+#'
+# ---- knit-int, echo=FALSE, include=FALSE
+set.seed(7869670)
 
+#'
 #' ## Initalisation
 #' The needed packages are loaded.
 # ---- load-packages, include=TRUE
@@ -18,15 +28,14 @@
 #install.packages("GMCM")  # Uncomment to install the GMCM package
 library("GMCM")
 
+#'
 #' If **GMCM** is not installed, please uncomment the above line and rerun the script.
 #' This document is parameterized with the following parameters:
 # ---- params, results='hold', comment="", echo=FALSE
-cat("params <- ")
-cat(capture.output(dput(params)), sep = "\n")
+# cat("params <- ")
+# cat(capture.output(dput(params)), sep = "\n")
 
-
-
-
+#'
 #' ## Load data
 #' The data is loaded and the first rows are printed
 # ---- load-data, include=TRUE, echo=TRUE
@@ -34,36 +43,59 @@ ds <- read.table(file   = params$file,
                  header = params$header,
                  sep    = params$sep,
                  quote  = params$quote)
-head(ds)
+head(ds, n = 4)
+
+#' Next, the columns of interest are selected and the data subsetted
+# ---- select-data, include=TRUE, echo=TRUE
+x <- ds[, params$model_cols]
+head(x, n = 2)
+
+#'
+#' ## Initial parameters
+#' The inital parameters are given by
+# ---- show-initial-params, include=TRUE, echo=TRUE
+theta <- as.theta(params$theta)
+print(theta)
 
 
-
+#'
 #' ## Model fitting
-#' With the data loaded, the model can now be fitted.
-#' However, some inital parameters are needed
+#' With the data loaded and defined initial parameters, the model is now fitted.
+# ---- fit_model, error=TRUE
+theta <- fit.full.GMCM(u = x,  # Ranking function is applied automatically
+                       theta = theta,
+                       method = params$fit_method,
+                       max.ite = params$max_ite,
+                       verbose = FALSE)
+print(theta)
 
+#' The fitting method is set `r params$fit_method` with a maximum number of interations of `r params$max_ite`.
+#'
 #' ## Unsupervised clustering
+#' The estimated parameters are used to calculated posterior component probabilities on which the classification is based:
+# ---- compute_probs
+kappa <- get.prob(x, theta)
+comps <- apply(kappa, 1, which.max)
 
+if (params$full_class_type == "thres_prob") {
+  ok_max <- apply(kappa, 1, max) > params$full_thres_prob
+  comps[!ok_max] <- NA
+}
+
+head(cbind(kappa, comp = comps))
 
 #' ## Results
+#' The results are displayed by plotting
+# ---- plot_results
+plot(x, col = comps)
 
-
-
-
-
-
-
-
-
-
-
-
+#'
 #' ## References
 
 # ---- citation, echo=FALSE, results='asis'
 cat("1. ", format(citation("GMCM"), style = "text"))
 
-#' ## Session information
-
+#' ### Session information
+#' This report was generated using `rmarkdown` and `knitr` with the following session information.
 # ---- session-info
 sessionInfo()
