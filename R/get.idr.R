8205fd70d1138692f0db0a7c5efcfb73f4304d15
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-05-22 20:36:15 +0200

    Removed get.idr from get.IDR docs. Fixes #43.

--- R/get.idr.R
+++ R/get.idr.R
@@ -1,12 +1,11 @@
-#' @rdname get.IDR
+#' @name get.IDR
+#' @usage NULL
 #' @note
-#'   \code{get.idr} returns a vector where the \eqn{i}'th entry is the
-#'   posterior probability that observation \eqn{i} is irreproducible.
-#'   It is a simple wrapper for \code{get.prob}.
-#'   From \pkg{GMCM} version 1.1 it has been an internal function.
-#'   Use \code{get.prop} or
-#'   \code{get.IDR} instead. The function can still be accessed with
-#'   \code{GMCM:::get.idr}.
-get.idr <- function (x, theta, ...) {
+#'   From \pkg{GMCM} version 1.1 \code{get.IDR} has been an internal function.
+#'   Use \code{get.prop} or \code{get.IDR} instead. The function can still be
+#'   accessed with \code{GMCM:::get.idr}. \code{get.idr} returns a vector where
+#'   the \eqn{i}'th entry is the posterior probability that observation \eqn{i}
+#'   is irreproducible. It is a simple wrapper for \code{get.prob}.
+get.idr <- function(x, theta, ...) {
   return(get.prob(Uhat(x), theta, ...)[, 1])
 }
