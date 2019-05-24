3b9bb9642da5f34e97a569f048c278c52f2d691f
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-05-24 12:55:39 +0200

    Described second usage in docs.

--- R/classify.R
+++ R/classify.R
@@ -2,10 +2,15 @@
 #'
 #' Classify observations according to the maximum a posterior probabilites.
 #'
-#' @inheritParams get.IDR
+#' @param x A \code{matrix} of A) observations where rows corresponds to
+#'   features and columns to studies or B) class probabilities with
+#' @param theta A list of parameters for the full model as described in
+#'   \code{\link{rtheta}}. If \code{theta} is supplied, \code{x} are assumed to
+#'   be observations (A). If \code{theta} is missing, \code{x} are assumed to be
+#'   probabilites (B).
 #'
-#' @return
-#' A integer vector of class numbers.
+#' @return A integer vector of class numbers with length equal to the number of
+#'   rows in \code{x}.
 #'
 #' @examples
 #' set.seed(4821)
@@ -16,6 +21,7 @@
 #' @export
 classify <- function(x, theta) {
   if (missing(theta)) {
+    stopifnot(all(0 <= x & x <= 1))
     kappa <- x
   } else {
     kappa <- get.prob(x, theta)
