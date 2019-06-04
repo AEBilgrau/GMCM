d93fff6d72d3e951d41b7b14f4c7097ecc6f8050
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-06-04 19:33:26 +0200

    Convert to meta report to Rmd format intented to use with knitr::knit_expand.

--- inst/shiny/run-reports.R
+++ inst/shiny/run-reports.R
@@ -3,33 +3,49 @@ library("rmarkdown")
 library("GMCM")
 
 # GENERAL GMCM ----
-report_path <- "inst/shiny/www/report_full.R"
+report_path <- "inst/shiny/www/report_full.Rmd"
 
 # SPECIAL GMCM ----
-report_path <- "inst/shiny/www/report_meta.R"
+report_path <- "inst/shiny/www/report_meta.Rmd"
 
-# Run directly
-rmarkdown::render(report_path,
-                  envir = new.env(parent = globalenv()))
+# Parameters to expand
+params <- list(input_file = "../../../data/u133VsExon.csv",
+               header = TRUE,
+               sep = ";",
+               quote = '\"',
+               model_cols = c("u133", "exon"),
+               meta_large_vals = FALSE,
+               init_par = c(pie1 = 0.5, mu = 1, sigma = 1, rho = 0.5),
+               meta_method = "NM",
+               meta_max_ite = 50L,
+               meta_positive_rho = TRUE,
+               meta_IDR_thres_type = "IDR",
+               meta_IDR_thres = 1e-04)
+
+# dput object to character string
+cput <- function(x) {
+  f <- tempfile()
+  dput(x, file = f)
+  return(readLines(f))
+}
 
 
-# Step-by-step
-# Spin document into Rmd
-rmd <- spin(hair = report_path, knit = FALSE, format = "Rmd")
+expand_args <- c(list(file = report_path), params)
+report_expanded <- do.call(knitr::knit_expand, expand_args)
+report_expanded_path <- gsub("/report_", "/report_expanded_", report_path)
 
-# Read yaml
-yaml <- rmarkdown::yaml_front_matter(rmd)
-params <- yaml$params
+cat(report_expanded, file = report_expanded_path)
 
-
+# Render the expanded document
 rmarkdown::render(
-  input = rmd,
+  input = report_expanded_path,
   output_options = list(self_contained = TRUE),
   params = params,
   envir = new.env(parent = globalenv())
 )
 
 # We can also purl
-knitr::purl(rmd, output = file.path(dirname(rmd), "report_full_purl.R"), documentation = 0)
+knitr::purl(report_expanded_path, output = gsub(".Rmd$", ".R", report_expanded_path),
+            documentation = 0)
 
 
