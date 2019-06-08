6a1bf5fe2678bba394a907746dd05101cc23ffcb
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-06-08 20:44:41 +0200

    Correct usage of no expected error.

--- tests/testthat/test-shiny-reports.R
+++ tests/testthat/test-shiny-reports.R
@@ -3,14 +3,11 @@ context("Test that shiny reports are working")
 test_that("shiny reports can be expanded and rendered", {
   skip_on_cran()
 
-  # Expect no errors
-  expect_failure(
-    expect_error(
-      nil <- capture_messages(capture_output( # Make render shut up
-        source(system.file("shiny/run-reports.R", package = "GMCM"))
-      ))
-    )
-  )
+  # Expect no errors (i.e. fail to provide errors)
+  expect_error(
+    nil <- capture_messages(capture_output( # Make render shut up
+      source(system.file("shiny/run-reports.R", package = "GMCM"))
+    )), NA)
 })
 
 
