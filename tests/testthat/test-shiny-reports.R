7c6a5daafc99828f1682018d741d41f52f35c9d7
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-06-11 20:29:27 +0200

    Show output.

--- tests/testthat/test-shiny-reports.R
+++ tests/testthat/test-shiny-reports.R
@@ -5,9 +5,9 @@ test_that("shiny reports can be expanded and rendered", {
 
   # Expect no errors (i.e. fail to provide errors)
   expect_error(
-    capture_messages(capture_output( # Make render shut up
-      source(system.file("shiny/run-reports.R", package = "GMCM"))
-    )),
+    # capture_messages(capture_output( # Make render shut up
+      source(system.file("shiny/run-reports.R", package = "GMCM")),
+    # ))
     NA
   )
 })
