c3db459dcd021ea9a11f0a70a100e93e73f7bd1c
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-06-12 20:30:30 +0200

    Attempt to get more output.

--- tests/testthat/test-shiny-reports.R
+++ tests/testthat/test-shiny-reports.R
@@ -2,11 +2,11 @@ context("Test that shiny reports are working")
 
 test_that("shiny reports can be expanded and rendered", {
   skip_on_cran()
-
+  source(system.file("shiny/run-reports.R", package = "GMCM"), echo = TRUE)
   # Expect no errors (i.e. fail to provide errors)
   expect_error(
     # capture_messages(capture_output( # Make render shut up
-      source(system.file("shiny/run-reports.R", package = "GMCM")),
+      source(system.file("shiny/run-reports.R", package = "GMCM"))$value,
     # ))
     NA
   )
