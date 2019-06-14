3128c0ce2bc188260c52a962c8d245488e378a00
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2019-06-14 20:00:26 +0200

    Fixed wrong call.

--- tests/testthat/test-shiny-reports.R
+++ tests/testthat/test-shiny-reports.R
@@ -4,8 +4,8 @@ test_that("shiny reports can be expanded and rendered", {
   skip_on_cran()
 
   # Expect no errors (i.e. fail to provide errors)
-  expect_error(GMCM:::run_meta_reports(), NA)
-  expect_error(GMCM:::run_full_reports(), NA)
+  expect_error(GMCM:::run_meta_report(), NA)
+  expect_error(GMCM:::run_full_report(), NA)
 })
 
 
