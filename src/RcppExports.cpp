dce017d7be16c9cdf26565f78162e7dbe0619c73
Anders E Bilgrau <anders.ellern.bilgrau@gmail.com>
2021-11-04 23:28:46 +0100

    New Roxygen/Rcpp version

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // colSdsArma
 arma::rowvec colSdsArma(const arma::mat& X, const int norm_type);
 RcppExport SEXP _GMCM_colSdsArma(SEXP XSEXP, SEXP norm_typeSEXP) {
